dc53244de962836b99adccb940e2ee3deabca2fe
Rassim Medkour <racimmedkour@gmail.com>
2023-08-21 17:15:33 +0100

    Added Routes file

---
+++ cmd/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func routes() http.Handler {
+
+	mux := chi.NewRouter()
+
+	// Create a new CORS handler
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowedHeaders:   []string{"Content-Type", "Accept", "Authorization", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	// Apply the CORS middleware to the router
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Use(corsHandler.Handler)
+
+	// Add your routes here
+	// Example: mux.Get("/", myHandlerFunc)
+
+	return mux
+}
